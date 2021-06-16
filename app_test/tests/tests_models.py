c3320d85071faf290f430fb74b57524fdf2d57c0
vgnatyuk <gnatyuk@forum-gd.ru>
2021-06-16 15:07:18 +0500

    added simple tests

---
+++ app_test/tests/tests_models.py
@@ -0,0 +1,15 @@
+from django.test import TestCase
+from ..models import TestModel, AnotherModel
+
+class ModelsTest(TestCase):
+    def test_testmodel_create(self):
+        counter = TestModel.objects.all().count()
+        TestModel.objects.create(name='test_name', apartment_id=10)
+        self.assertEqual(TestModel.objects.all().count(), counter + 1)
+
+    def test_anothermodel_create(self):
+        counter = AnotherModel.objects.all().count()
+        print(counter)
+        AnotherModel.objects.create(another_name='another_test_name', another_id='id')
+        print(AnotherModel.objects.all().count())
+        self.assertEqual(AnotherModel.objects.all().count(), counter + 1)
